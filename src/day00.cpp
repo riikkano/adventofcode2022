eeae5962afc618f2a59fb168a3ecb889bfcb5ed5
Riikka <riikkan0@gmail.com>
2022-11-25 22:53:28 +0200

    Solve 2021 day 1 pt.1

--- src/day00.cpp
+++ src/day00.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(){
-    std::cout << "Hello!";
+    std::string line; 
+
+    std::ifstream MyReadFile("inputs/day00.txt");
+
+    std::getline(MyReadFile,line);
+    int32_t previousReading = std::stoi(line);
+    std::cout << line << " (N/A - no previous measurement)\n";
+   
+    int32_t counter = 0;
+
+    while (std::getline(MyReadFile, line)){
+        int32_t reading = std::stoi(line);
+        if (reading > previousReading) {
+            std::cout << line << " (increased)\n";
+            counter ++;
+        }
+        if (reading < previousReading) {
+            std::cout << line << " (decreased)\n";
+        }
+        previousReading = reading;
+    }
+     std::cout << "No. larger than the previous measurement: " << counter;
     return 0;
 }
