0e36a780f15b829a96cd5ab17b89eba8502cf4e6
Riikka <riikkan0@gmail.com>
2022-11-26 13:29:31 +0200

    Solve 2021 day 1 pt.2

--- src/day00.cpp
+++ src/day00.cpp
@@ -6,24 +6,44 @@ int main(){
     std::string line; 
 
     std::ifstream MyReadFile("inputs/day00.txt");
-
-    std::getline(MyReadFile,line);
-    int32_t previousReading = std::stoi(line);
-    std::cout << line << " (N/A - no previous measurement)\n";
    
     int32_t counter = 0;
 
+    std::deque<int> values = {0,0,0,0};
+
+    // Read first window
+    for (int i=0; i < 3; i++) {
+        std::getline(MyReadFile,line);
+        int32_t reading = std::stoi(line);
+        values.push_front(reading);
+        values.pop_back();
+    }
+
+    // Initiate previous sum
+    int previousSum = values[0] + values[1] + values[2];
+
+    std::cout << previousSum << " (N/A - no previous measurement)\n";
+
+
     while (std::getline(MyReadFile, line)){
         int32_t reading = std::stoi(line);
-        if (reading > previousReading) {
-            std::cout << line << " (increased)\n";
+        values.push_front(reading);
+        values.pop_back();
+
+        int sum = values[0] + values[1] + values[2];
+
+        if (sum > previousSum) {
+            std::cout << sum << " (increased)\n";
             counter ++;
         }
-        if (reading < previousReading) {
-            std::cout << line << " (decreased)\n";
+        if (sum < previousSum) {
+            std::cout << sum << " (decreased)\n";
         }
-        previousReading = reading;
+        if (sum == previousSum){
+            std::cout << sum << " (no change)\n";
+        }
+        previousSum = sum;
     }
-     std::cout << "No. larger than the previous measurement: " << counter;
+     std::cout << "Number of larger than the previous sums: " << counter;
     return 0;
 }
