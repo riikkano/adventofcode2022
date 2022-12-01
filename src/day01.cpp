11b272334586eed8b10b302f90923e7ce67d111a
Riikka <riikkan0@gmail.com>
2022-12-01 07:22:13 +0200

    completed day1 part 1

--- src/day01.cpp
+++ src/day01.cpp
@@ -1,11 +1,31 @@
+#include <algorithm>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main()
 {
     std::ifstream MyReadFile("inputs/day01.txt");
     std::string line;
-    std::getline(MyReadFile, line);
-    std::cout << line;
+    std::vector<uint32_t> sums = {};
+
+    int32_t sum = 0;
+    while (std::getline(MyReadFile, line))
+    {
+        if (line.empty())
+        {
+            sums.push_back(sum);
+            sum = 0;
+        }
+        else
+        {
+            int32_t calories = std::stoi(line);
+            sum = sum + calories;
+        }
+    }
+
+    auto max = std::max_element(sums.begin(), sums.end());
+    std::cout << "hello";
 }
