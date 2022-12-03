969d06d42b80d5edf4a97a1bd4573cadb99c3d02
Riikka <riikkan0@gmail.com>
2022-12-03 11:25:47 +0200

    Comment out redundant headers

--- src/day.cpp
+++ src/day.cpp
@@ -1,12 +1,13 @@
-#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
+// #include <vector>
+// #include <algorithm>
+// #include <unordered_map>
 
 int main()
 {
-    std::ifstream MyReadFile("inputs/day03.txt");
+    std::ifstream MyReadFile("inputs/day.txt");
     std::string line;
     std::getline(MyReadFile, line);
     std::cout << line;
