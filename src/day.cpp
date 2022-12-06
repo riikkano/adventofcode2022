617adc63d8d266d968048eddcc0321ffa3291f04
Riikka <riikkan0@gmail.com>
2022-12-06 08:47:16 +0200

    Add convenience function to template

--- src/day.cpp
+++ src/day.cpp
@@ -1,9 +1,31 @@
+
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <string>
 // #include <vector>
 // #include <algorithm>
 // #include <unordered_map>
+
+std::list<std::string> split(std::string s, char delimiter)
+{
+    std::list<std::string> items;
+    std::string item{};
+    for (char c : s)
+    {
+        if (c != delimiter)
+        {
+            item.push_back(c);
+        }
+        else if (c == delimiter)
+        {
+            items.push_back(item);
+            item.clear();
+        }
+    }
+    items.push_back(item); // add last item
+    return items;
+}
 
 int main()
 {
@@ -11,4 +33,10 @@ int main()
     std::string line;
     std::getline(MyReadFile, line);
     std::cout << line;
+    std::string s{"hek,kasi,adoa,23,sjdas,123"};
+    auto splitted = split(s, ',');
+    for (auto i : splitted)
+    {
+        std::cout << i << std::endl;
+    }
 }
